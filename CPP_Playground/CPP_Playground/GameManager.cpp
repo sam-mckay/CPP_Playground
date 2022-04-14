242a38247bcc30b132fbcf97814cfdd033a35fc0
Sam <sammckay@btinternet.com>
2022-04-14 15:01:49 +0100

    feat(GameManager): Added game manager logic

--- CPP_Playground/CPP_Playground/GameManager.cpp
+++ CPP_Playground/CPP_Playground/GameManager.cpp
@@ -1,1 +1,58 @@
 #include "GameManager.h"
+
+BattleshipGame::GameManager::GameManager()
+{
+	isActive = false;
+	player = Player();
+	aiPlayer = EnemyPlayer();
+	turn = Turn::Player1;	
+}
+
+void BattleshipGame::GameManager::NextTurn()
+{
+	if (turnCount > turnCap)
+	{
+		isActive = false;
+		cout << "Game Ended as it took too many turns" << endl;
+	}
+
+
+	Coordinate selectedTarget;
+	list<Battleship> ships;
+
+	switch (turn)
+	{
+	case Turn::Player1:
+		selectedTarget = player.TakeTurn();		
+		ships = player.ships;
+		break;
+	case Turn::Player2:
+		selectedTarget = aiPlayer.TakeTurn();		
+		ships = aiPlayer.ships;
+		break;
+	default:
+		break;
+	}
+
+	HandleHit(selectedTarget, ships);
+
+
+	turnCount++;
+}
+
+void BattleshipGame::GameManager::NewGame(int shipCount, int boardX, int boardY, int _turnCap)
+{
+	isActive = true;
+	BattleshipBoard playerBoard = BattleshipBoard(boardX, boardY);
+	BattleshipBoard aiBoard = BattleshipBoard(boardX, boardY);
+
+	player = Player(playerBoard);
+	aiPlayer = EnemyPlayer(aiBoard);
+
+	turnCap = _turnCap;
+}
+
+void BattleshipGame::GameManager::HandleHit(Coordinate target, list<Battleship> ships)
+{
+
+}
--- CPP_Playground/CPP_Playground/GameManager.h
+++ CPP_Playground/CPP_Playground/GameManager.h
@@ -1,14 +1,25 @@
 #pragma once
 #include "Player.h"
+#include "EnemyPlayer.h"
+#include "Turn.h"
 
 namespace BattleshipGame
 {
 	class GameManager
 	{
 	public:
-		bool isStarted;
+		bool isActive;
 		Player player;
-		Player aiPlayer;
+		EnemyPlayer aiPlayer;
+		int turnCap;
+		int turnCount;
 
+		Turn turn;
+
+		GameManager();
+
+		void NextTurn();
+		void NewGame(int shipCount, int boardX, int boardY, int _turnCap = 10);
+		void HandleHit(Coordinate target, list<Battleship> ships);
 	};
 }
